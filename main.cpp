98867e27ff808e910ab12ae3af7949abca367943
fireslash88 <staniscia.alessandro09@gmail.com>
2026-07-04 04:25:02 +0200

    Quick fixes Cleaned up some code Added some descriptions Trying a new way to add a delay...

--- main.cpp
+++ main.cpp
@@ -11,37 +11,43 @@ struct Values {
 };
 
 //Constants Variables
-const int screenWidth=1080;
-const int screenHeight=720;
-const int maxValues=100;
+const int screenWidth = 1080;
+const int screenHeight = 720;
+const int maxValues = 100;
 
 //Global variables
 Values value[maxValues];
-int shownValues=5;
-int newValues=0;
+int shownValues = 5;
+int newValues = 0;
+
+float frameTime = 0.0;
 
 bool ValueBox1EM = false;
 
 int ListViewIndexSel;
-int ListViewIndexActive=-1;
-
-float frameCounter=0;
+int ListViewIndexActive = -1;
 
 //Main Functions
 void InitGame();
+
 void UpdateGame();
+
 void DrawGame();
+
 void UnloadGame();
+
 void UpdateDrawFrame();
 
 //Other functions
-void randomize();
+void shuffle();
+
+bool delay(float seconds);
 
 //Algorithms functions
 void bubbleSort();
 
 int main() {
-    InitWindow( screenWidth, screenHeight, "Template Raylib");
+    InitWindow(screenWidth, screenHeight, "Template Raylib");
     InitGame();
     SetTargetFPS(60);
 
@@ -56,67 +62,76 @@ int main() {
 
 //Initiate game variables;
 void InitGame() {
-    float rectSize=static_cast<float>(screenWidth)/static_cast<float>(shownValues);
-    for (int i=0;i<shownValues;i++) {
-        value[i].x=rectSize*static_cast<float>(i);
-        value[i].y=300;
-        value[i].width=rectSize;
-        value[i].height=0;
-    }
-    randomize();
 
-    ValueBox1EM = false;
+    // Calculate how large is the width of every rectangle
+    float rectSize = static_cast<float>(screenWidth) / static_cast<float>(shownValues);
+
+    // Initialize every rectangle of the values with the calculated width and some base settings
+    for (int i = 0; i < shownValues; i++) {
+        value[i].x = rectSize * static_cast<float>(i);
+        value[i].y = 300;
+        value[i].width = rectSize;
+        value[i].height = 0;
+    }
+
+    // Shuffle the values at the start of the program
+    shuffle();
 }
 
 //Update Game
 void UpdateGame() {
+
+    // TEST ONLY. Press space to sort, only for test
     if (IsKeyPressed(KEY_SPACE)) {
+        // delay(5.0);
         bubbleSort();
     }
 
-    if (frameCounter<=5.0) {
-        frameCounter+=GetFrameTime();
-    }
+    frameTime += GetFrameTime();
 }
 
 //Draw game
 void DrawGame() {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    GuiSetStyle(DEFAULT,TEXT_SIZE,20);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
 
     // Draws all the values as rectangles, uses Vectors.
-    for (int i=0;i<shownValues;i++) {
-        Vector2 position = { value[i].x,value[i].y-value[i].height};
-        Vector2 size = { value[i].width,value[i].height};
+    for (int i = 0; i < shownValues; i++) {
+        Vector2 position = {value[i].x, value[i].y - value[i].height};
+        Vector2 size = {value[i].width, value[i].height};
 
-        DrawRectangleV(position,size,RED);
-        DrawText(TextFormat("%.0f",value[i].height),value[i].x,value[i].y,20,BLACK);
+        DrawRectangleV(position, size,RED);
+
+        // !! TO DELETE shows value of the rectangle's height
+        DrawText(TextFormat("%.0f", value[i].height), value[i].x, value[i].y, 20,BLACK);
     }
 
-    // Draws box to insert the quantity of values to sort
-    if (GuiValueBox({200,350,60,20},"Values to sort",&newValues,5,maxValues,ValueBox1EM)) {
-        ValueBox1EM=!ValueBox1EM;
+    // Draws box to insert the quantity of values to sort, minimum quantity of values is 5, maximum is decided by the constant. !!SHOULD ADD CONSTANT FOR MINIMUM TOO
+    if (GuiValueBox({200, 350, 60, 20}, "Values to sort", &newValues, 5, maxValues, ValueBox1EM)) {
+        ValueBox1EM = !ValueBox1EM;
         if (!ValueBox1EM) {
-            shownValues=newValues;
+            shownValues = newValues;
             InitGame();
         }
     }
 
     // Draws list view of all the algorithms available
-    GuiListView({700,350,200,150},"Bubble sort;Test2;Test3",&ListViewIndexSel,&ListViewIndexActive);
-    if (ListViewIndexActive==-1) {
-        DrawText(TextFormat("No algorithm selected"),50,50,20,RED);
-    }
-    else {
-        DrawText(TextFormat("Value: %d",ListViewIndexActive),50,50,20,RED);
-    }
-
-    if (GuiButton({300,350,150,50},"Shuffle")) {
-        randomize();
+    GuiListView({700, 350, 200, 150}, "Bubble sort;Test2;Test3", &ListViewIndexSel, &ListViewIndexActive);
+    if (ListViewIndexActive == -1) {
+        DrawText(TextFormat("No algorithm selected"), 50, 50, 20,RED);
+    } else {
+        DrawText(TextFormat("Value: %d", ListViewIndexActive), 50, 50, 20,RED);
     }
 
-    DrawText(TextFormat("DeltaTime: %f",frameCounter),300,600, 20, RED);
+    // Draws button to shuffle the rectangles
+    if (GuiButton({300, 350, 150, 50}, "Shuffle")) {
+        shuffle();
+    }
+
+
+    // TO DELETE, just for testing purpose
+    DrawText(TextFormat("DeltaTime: %f", frameTime), 300, 600, 20, RED);
 
     EndDrawing();
 }
@@ -131,16 +146,28 @@ void UpdateDrawFrame() {
     DrawGame();
 }
 
-void randomize() {
-    for (int i=0;i<shownValues;i++) {
-        value[i].height=(GetRandomValue(1,100));
+//Shuffle the rectangle positions
+void shuffle() {
+    for (int i = 0; i < shownValues; i++) {
+        value[i].height = (GetRandomValue(1, 100));
     }
 }
 
+// Adds a delay between swaps
+bool delay(float seconds) {
+
+    if (seconds <= frameTime) {
+        frameTime = 0;
+        return true;
+    }
+    return false;
+}
+
+
 void bubbleSort() {
-    bool swapped=false;
-    for (int i=0;i<shownValues-1;i++) {
-        for (int j=0;j<shownValues-1-i;j++) {
+    bool swapped = false;
+    for (int i = 0; i < shownValues - 1; i++) {
+        for (int j = 0; j < shownValues - 1 - i; j++) {
             if (value[j].height > value[j + 1].height) {
                 float temp = value[j].height;
                 value[j].height = value[j + 1].height;
@@ -151,7 +178,6 @@ void bubbleSort() {
         if (!swapped) {
             break;
         }
-        swapped=false;
+        swapped = false;
     }
-
 }
